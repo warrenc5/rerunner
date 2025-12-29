2e94571c75d912811c6bd12e0032349e2b662fac
Warren Crossing <warren@itcl.com.au>
2025-12-29 21:01:00 +1100

    update exit signals to be passed to slave

--- runner.sh
+++ runner.sh
@@ -1,17 +1,57 @@
 #!/bin/bash 
 #set -x
 #cd `dirname $0`
-EXIT=${EXIT:-(123 0)}
-#CONT=(123 0 131 130)
-CONT=(123 0 131 130)
-EXIT=(137)
+
+CONT=(0 123 124 131) #xargs runner will continue
+EXIT=(1 126 127) #xargs - runner will exit
+
 MY_PID=$$
+DRY_RUN=${DRY:-}
+VERBOSE=1
 xpatterns=("\.sw.*" "\.netbeans_automatic_build")
+
+RED="\e[31m"
+BOLD="\e[1m"
+GREEN="\e[32m"
+BLUE="\e[34m"
+RESET="\e[0m"
+CYAN="\e[36m"
+MAGENTA="\e[35m"
+YELLOW="\e[33m"
+WHITE="\e[97m"
+BLACK="\e[30m"
+ITALICS="\e[3m"
+UNDERLINE="\e[4m"
+RESET="\e[0m"
+BOLD="\e[1m"
+ITALICS="\e[3m"
+UNDERLINE="\e[4m"
+RED="\e[31m"
+GREEN="\e[32m"
+ORANGE="\e[33m"
+BLUE="\e[34m"
+CYAN="\e[36m"
+MAGENTA="\e[35m"
+YELLOW="\e[33m"
+WHITE="\e[97m"
+BLACK="\e[30m"
+
+DATE="date --rfc-3339=seconds"
+DATE2="date +%Y%m%d%H%M%S"
+
+echo -e $RESET
 echo "my pid is $MY_PID"
-PROG=$@
+[[ $VERBOSE -eq 1 && ${DRY_RUN} -ne "" ]] && echo -e "${YELLOW}==============DRY_RUN============${RESET}" 
+
+MAX=200
+PROG=$@ #Program to run with arguments
+PROG_CUT=${PROG[@]}
+PROG_CUT=${PROG_CUT:0:200}
+echo -e ${WHITE}${PROG_CUT}${RESET}
 
 if [ -z "$1" ] ; then 
-echo 'traps are CTRL+\ to rerun and CTRL+C to int'
+echo 'traps are CTRL+\ to rerun, CTRL+C to int, CTRL+Z hup exit'
+echo 'during program run traps CTRL+\ CTRL+C are passed through, CTRL+Z hup exit'
 echo 'usage runner.sh target.sh // by default it will watch target.sh'
 echo 'usage runner.sh target.sh <<<`find . -name \*.java -o -name \*.js -o -name \*.xml | grep -v "test\|target"`'
 echo 'usage runner.sh target.sh <<<$(ls this file and that file)'
@@ -33,24 +73,15 @@ fi
 while IFS=';' read -t 1 -ra input && [ -n "$input" ]; do 
   #echo watching $input
   WATCH+=(${input[@]})
-
 done
 
-
-if [ 0 -eq ${#WATCH[@]} ] ; then 
-  #echo 'default watching target'
-  for arg in $@ ; do
-    r=`realpath $arg`
-    WATCH+=($r)
-  done
-fi 
-
 filtered=()
+
 for item in "${WATCH[@]}"; do
   m=0
   for pat in "${xpatterns[@]}"; do
     if [[ $item =~ $pat ]]; then
-        echo "remove: $item $pat"
+        [[ $VERBOSE -eq 1 ]] && echo "remove: $item $pat"
         m=1
         break;
     fi
@@ -62,16 +93,24 @@ done
 
 WATCH=("${filtered[@]}")
 
-echo "watching ${WATCH[@]}"         # print array elements
+if [[ 0 -eq ${#WATCH[@]} ]] ; then 
+  echo 'default - watching target'
+  for arg in $@ ; do
+    r=`realpath $arg`
+    WATCH+=($r)
+  done
+
+  #[[ $VERBOSE -eq 1 ]] && echo -e "${ITALICS}input ${WATCH[@]}${RESET}"         # print array elements
+fi 
+
+WATCH_CUT=${WATCH[@]}
+WATCH_CUT=${WATCH_CUT:0:200}
+
 echo "watching ${#WATCH[@]} files"        # print array length
 
-#for file in "${WATCH[@]}"; do echo "$file"; done  # loop over the array
+echo -e "${ITALICS}watching filtered ${WATCH_CUT}${RESET}"         # print array elements
+#[[ $VERBOSE -eq 1 ]] && echo -e "${ITALICS}watching filtered ${WATCH[@]}${RESET}"         # print array elements
 
-IPROG=inotifywait 
-# todo inotifywait
-
-#ARGS="-o /tmp/inotify-${MY_PID} -d -r -"
-ARGS="-e modify -e create"
 LOCK_DIR=/var/run/lock/
 
 if [ ! -d $LOCK_DIR ] ; then
@@ -82,12 +121,24 @@ PIDF=${LOCK_DIR}/runner.pid
 #echo $MY_PID > ${PIDF}
 echo ${MY_PID} ${PIDF}
 
+IPROG=inotifywait 
 which $IPROG > /dev/null 2>&1
 
-if [ $? -ne 0 ] ; then
+#ARGS="-o /tmp/inotify-${MY_PID} -d -r -"
+
+EVENTS=(modify attrib create)
+
+for event in "${EVENTS[@]}"; do 
+  ARGS="$ARGS -e ${event}"
+done  
+
+if [ $? -ne 0 ] ; then # fallback to fswatch
   IPROG=fswatch
   ARGS="-1 -l 1 "
 fi
+
+XARGS="xargs ${IPROG} ${ARGS}"
+#[[ $VERBOSE -eq 1 ]] XARGS="xargs -t ${IPROG} ${ARGS}"
 
 if [ $? == 127 ] ; then
   echo "sudo apt install inotify-tools"
@@ -108,28 +159,61 @@ echo "starting with pid date ${DATE}"
 #set -o ignoreeof
 #CTRL+\
 
-trap "echo 'quit1' && touch -d ${DATE} ${PIDF} && pkill -HUP ${IPROG}" QUIT
-trap "echo 'exit1' && pkill -HUP ${IPROG}" EXIT
-trap "echo 'hup' && pkill -HUP ${IPROG}" SIGHUP
-trap "echo 'abort' && pkill -ABRT ${IPROG}" SIGABRT
-
 let err=0
+
+function update() { 
+touch -d ${DATE} ${PIDF}
+}
+
+function traps() { 
+set +x
+  trap "echo -e \"${WHITE}${BOLD}trap quit${RESET}\" && update && kill -HUP ${PID}" QUIT #CTRL-\
+  trap "echo -e \"${WHITE}${BOLD}trap exit${RESET}\" && kill -HUP ${PID} ; kill -- $$" EXIT 
+  trap "echo -e \"${WHITE}${BOLD}trap hup${RESET}\" && kill -HUP ${PID}" SIGHUP #129
+  trap "echo -e \"${WHITE}${BOLD}trap abort'${RESET}\" && kill -ABRT ${PID}" SIGABRT
+  trap "echo -e \"${WHITE}${BOLD}trap int${RESET}\" && kill -HUP ${MY_PID}" INT #CTRL-C #130
+  trap "echo -e \"${WHITE}${BOLD}trap stop${RESET}\" && kill -HUP ${MY_PID}" TSTP #CTRL-Z
+}
+
+traps
 
 function run () { 
   touch $PIDF
-  ${PROG} &
+  echo "------------------------>" 
+
+  if [[ -z $DRY_RUN ]] ; then 
+    ${PROG} &
+  else 
+    [[ $VERBOSE -eq 1 ]] && echo -e ${YELLOW}${PROG_CUT}${RESET} 
+    sleep 10
+  fi 
+  
   PID=$!
-  trap "echo 'int ${PROG}' && kill -9 ${PID}" INT
+  #send signals to prog
+  trap "echo -e \"${WHITE}${UNDERLINE}trap int ${PROG}${RESET}\" && traps && kill -TERM ${PID}" INT #CTRL-C
+  trap "echo -e \"${WHITE}${UNDERLINE}trap quit${RESET}\" && traps && kill -QUIT ${PID}" QUIT #CTRL-\
+  trap "echo -e \"${WHITE}${UNDERLINE}trap stop${RESET}\" && traps && kill -TERM ${PID}" TSTP #CTRL-Z
+
   #echo "RUNNING ${PROG} $PID `date -Iseconds`" 
-  echo "RUNNING '${PROG}' pid: $PID time: `date -u +'%Y-%m-%dT%H:%M:%S'`"
+  echo -e "${WHITE}${UNDERLINE}RUNNING${UNDERLINE} '${PROG}' ${ITALICS}pid: $PID time: `date -u +'%Y-%m-%dT%H:%M:%S'` ${RESET}"
   echo -n $PID > $LOCK_DIR/runner-cmd.pid
   wait $PID 
   RET=$?
+  traps
 
-  echo "completed - return code $RET"
+  echo "<------------------------"
 
-  if [ "$RET" != 0 ] ; then
-    echo "command failed"
+  if [ $RET -eq 0 ] ; then
+    echo -e ${GREEN}${BOLD}${PROG} $PID - completed - return code:${ITALICS} ${RET}${RESET}
+  elif (( $RET > 128 )); then 
+    sig=$(($RET-128)); 
+    echo -e ${WHITE}${BOLD}${PROG} $PID was KILLED $sig $(kill -l $sig) ${RESET} 
+    if [[ $sig -eq 20 ]] ; then
+      kill -HUP ${PID}
+    fi
+
+  else 
+    echo -e ${RED}${BOLD}${PROG} $PID - command failed - return code:${ITALICS} ${RET}${RESET}
   fi
 
 }
@@ -139,11 +223,11 @@ while [[ 1 ]]; do
 
   echo "checking up to date"
 
-  trap "echo 'int2' && kill -9 ${MY_PID}" INT
+  traps
 
   for file in ${WATCH[@]};  do
     if [ $file -nt $PIDF ] ; then
-      echo "building newer "
+      echo -e ${YELLOW}building newer $file${RESET}
       run
       break
     fi
@@ -151,25 +235,40 @@ while [[ 1 ]]; do
 
   #set -x
 
-  echo "${PROG} is waiting for notification to run"
-  echo "using ${IPROG} ${ARGS}"
-  set -x
-  echo ${WATCH[@]} | xargs -t ${IPROG} ${ARGS} &
+  echo -e "${PROG_CUT} ${WHITE}${ITALICS}is waiting for notification to run${RESET}"
+  echo -e "using ${ITALICS}${IPROG} ${ARGS}${RESET}"
   set +x
+
+  if [[ $VERBOSE -eq 1 ]] ; then
+    echo ${WATCH[@]} | $XARGS &
+  else 
+    echo ${WATCH[@]} | $XAGRS &
+  fi 
+
   PID=$!
-  echo $PID
+  traps 
+  sleep 1
+  [[ $VERBOSE -eq 1 ]] && echo -e ${ITALICS}watcher pid: $PID${RESET}
+  set -x
   wait $PID
   RET=$?
+
   echo "${IPROG} exited $RET"
 
-  if [[ " ${CONT[*]} " =~ " $RET " ]] ; then 
-    echo $IPROG success exit with $RET 
+  #XARGS exited
+
+  if [[ " ${CONT[*]} " =~ " $RET " ]] ; then  
+    echo -e ${GREEN}${UNDERLINE}$IPROG success exit with $RET ${RESET}
     run
+  elif (( $RET > 128 )); then 
+    sig=$(($RET-128)); 
+    echo -e ${WHITE}${BOLD}${IPROG} was KILLED $sig $(kill -l $sig) ${RESET} 
+    kill $SIG ${MY_PID}
   elif [[ " ${EXIT[*]} " =~ " $RET " ]] ; then 
-    echo $IPROG failed exit with $RET 
+    echo -e ${RED}${UNDERLINE}$IPROG failed exit with ${RET} ${RESET}
     exit $RET
   else
-    echo $IPROG failed with $RET - valid are ${EXIT[*]} and ${CONT[*]}
+    echo -e ${RED}${UNDERLINE}$IPROG failed with $RET - valid are ${EXIT[*]} and ${CONT[*]} ${RESET}
     exit 69
   fi
 done
